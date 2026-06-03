6a25e6cc86d9ea7ca866eea2d9021f764445a1d2
itzz <georg.ott.mail@gmail.com>
2026-06-03 19:33:26 +0200

    cooking

---
+++ src/GUI/Modules/MenuKeybind.hpp
@@ -0,0 +1,22 @@
+/*#pragma once
+
+#include "../../Client/KeybindModule.hpp"
+
+using namespace geode::prelude;
+
+class MenuKeybind : public KeybindModule
+{
+    public:
+        MODULE_SETUP(MenuKeybind)
+        {
+            setName("Menu Keybind:");
+            setID("internal/open-menu");
+            setDescription("");
+            setCategory("Config/Menu");
+
+            setDefaultKeyCode(enumKeyCodes::KEY_Tab); // Tab
+            setBindRequired(true);
+        }
+};
+
+SUBMIT_HACK(MenuKeybind);*/
---
+++ src/GUI/Modules/PopupUIScale.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "../../Client/FloatSliderModule.hpp"
+#include <PopupBase.hpp>
+
+using namespace geode::prelude;
+
+class PopupUIScale : public FloatSliderModule
+{
+    public:
+        MODULE_SETUP(PopupUIScale)
+        {
+            setID("popup-ui-scale");
+            setCategory("Config/Menu");
+            setDefaultValue(1);
+            setRange(0.6f, 1.0f);
+        }
+
+        virtual void onSliderEnded()
+        {
+            PopupBase::updateAllUIScale();
+        }
+};
