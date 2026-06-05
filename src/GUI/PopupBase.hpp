65e50c7ad23e7a89738547df9b40338c7021131b
itzz <georg.ott.mail@gmail.com>
2026-06-05 02:50:53 +0200

    cooking

---
+++ src/GUI/PopupBase.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+#include <ThemeManager.hpp>
+
+class PopupBase : public cocos2d::CCLayerColor, public cocos2d::CCActionTweenDelegate
+{
+    protected:
+        cocos2d::CCSize m_size;
+        bool m_noElasticity = false;
+        cocos2d::extension::CCScale9Sprite* m_bgSprite = nullptr;
+        cocos2d::CCMenu* m_buttonMenu = nullptr;
+        cocos2d::CCLayerColor* blurLayer = nullptr;
+        float drawOpacity = 1;
+        geode::Ref<cocos2d::CCRenderTexture> rt = nullptr;
+        int baseOpacity = 150;
+        cocos2d::CCNode* customBG = nullptr;
+
+        // pre 2.2081 compatibility
+        virtual void keyDown(cocos2d::enumKeyCodes code);
+        virtual void keyDown(cocos2d::enumKeyCodes code, double timestamp);
+        virtual void keyBackClicked();
+        virtual void registerWithTouchDispatcher(void);
+        
+        PopupBase();
+        ~PopupBase();
+
+        void onBtn1(CCObject* sender);
+        void onBtn2(CCObject* sender);
+
+        virtual void updateTweenAction(float value, const char* key);
+
+        float calcPopupScale();
+        void updateUIScale();
+        void updateBlur();
+
+    public:
+        bool addToScene = true;
+        bool shouldVisit = true;
+        cocos2d::CCLayer* m_mainLayer = nullptr;
+
+        static void updateAllUIScale();
+        static void updateAllBlur();
+
+        virtual bool init(float width, float height);
+        virtual bool initAnchored(float width, float height);
+        virtual bool setup();
+        virtual void show();
+        virtual void visit();
+        virtual void draw();
+
+        void playAnimation(qolmod::MenuAnimation anim);
+        virtual void onClose(CCObject* sender);
+};
---
+++ src/GUI/SearchFiltersUI.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+#include "BackgroundSprite.hpp"
+#include "../Client/Module.hpp"
+#include "../Client/ModuleNode.hpp"
+#include "../Client/CategoryNode.hpp"
+#include "PopupBase.hpp"
+
+using namespace geode::prelude;
+
+class SearchFiltersUI : public PopupBase
+{
+    protected:
+        qolmod::BackgroundSprite* bg;
+
+    public:
+        static SearchFiltersUI* create();
+
+        virtual bool setup();
+};
