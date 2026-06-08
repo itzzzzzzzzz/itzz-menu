6126eac5f3b4be82263e50afb9b25683e21b0f5b
itzz <georg.ott.mail@gmail.com>
2026-06-08 20:49:57 +0200

    cooking

---
+++ src/Keybinds/KeybindStruct.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+#include "../Client/KeybindModule.hpp"
+#include "KeyState.hpp"
+
+using namespace geode::prelude;
+
+struct KeyConfigState
+{
+    bool shiftRequired = false;
+    bool ctrlRequired = false;
+    bool altRequired = false;
+    bool cmdRequired = false;
+
+    enumKeyCodes code = enumKeyCodes::KEY_None;
+    KeybindModule* mod = nullptr;
+    bool activateOnDown = false;
+    bool activateOnRepeat = false;
+    bool activateOnRelease = false;
+};
+
+struct KeybindStruct
+{
+    std::string name;
+    std::string id;
+    KeyConfigState config;
+    bool allowInGameplay = true;
+    bool allowInEditor = true;
+    bool allowInMenus = true;
+    bool sendNotification = false;
+
+    bool isEligable();
+    bool canBeActivated(KeyState state);
+
+    virtual void onActivate(KeyState state) {}
+};
