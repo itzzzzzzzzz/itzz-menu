e772a52c5f896e2532d0457dd66625c227c89d22
itzz <georg.ott.mail@gmail.com>
2026-06-09 07:15:42 +0200

    cooking

---
+++ src/Labels/BestRun.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify (BestPlayLayer, PlayLayer)
+{
+	struct Fields {
+		float fromPercent;
+        float toPercent;
+
+        float bestFrom;
+        float bestTo;
+        float bestLength;
+
+        bool ignoreBest;
+
+        float lastPercent;
+	};
+
+	void resetLevel();
+    void levelComplete();
+    virtual void destroyPlayer(PlayerObject* p0, GameObject* p1);
+
+    std::string getRoundedString(float f);
+    std::string getRunString();
+};
---
+++ src/Labels/Hooks.cpp
@@ -0,0 +1,38 @@
+#include "Hooks.hpp"
+#include "Nodes/LabelContainerLayer.hpp"
+
+bool LabelPlayLayer::init(GJGameLevel* level, bool useReplay, bool dontCreateObjects)
+{
+    if (!PlayLayer::init(level, useReplay, dontCreateObjects))
+        return false;
+
+    m_uiLayer->addChild(LabelContainerLayer::create());
+
+    return true;
+}
+
+void LabelPlayLayer::resetLevel()
+{
+    PlayLayer::resetLevel();
+
+    if (LabelContainerLayer::get())
+        LabelContainerLayer::get()->onNewAttempt();
+}
+
+void LabelBaseGameLayer::handleButton(bool down, int button, bool isPlayer1)
+{
+    GJBaseGameLayer::handleButton(down, button, isPlayer1);
+
+    if (LabelContainerLayer::get() && down && button == 1)
+        LabelContainerLayer::get()->onPlayerClicked(isPlayer1 ? NoclipPlayerSelector::Player1 : NoclipPlayerSelector::Player2);
+
+    if (auto ll = LabelContainerLayer::get())
+    {
+        ll->onEventTriggered(down ? LabelEventType::ClickStarted : LabelEventType::ClickEnded);
+
+        if (isPlayer1)
+            ll->onEventTriggered(down ? LabelEventType::P1ClickStarted : LabelEventType::P1ClickEnded);
+        else
+            ll->onEventTriggered(down ? LabelEventType::P2ClickStarted : LabelEventType::P2ClickEnded);
+    }
+}
