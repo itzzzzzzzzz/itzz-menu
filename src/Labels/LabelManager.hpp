d3b6a76a284b0bc22d86594e7f58075c2db8f86e
itzz <georg.ott.mail@gmail.com>
2026-06-10 05:48:00 +0200

    cooking

---
+++ src/Labels/LabelManager.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "LabelConfig.hpp"
+#include <chrono>
+#include "../Client/Module.hpp"
+
+class LabelManager
+{
+    friend class SpeedhackScheduler;
+
+    protected:
+        std::vector<LabelConfig> configs = {};
+        std::chrono::steady_clock::time_point sessionBegin;
+        float fps;
+        float _updateInterval = 0.5f;
+        float _timeLeft = _updateInterval;
+        float _accum = 0;
+        int _frames = 0;
+        cocos2d::CCRect safeZone;
+
+        void update(float dt);
+
+    public:
+        static LabelManager* get();
+
+        const std::vector<LabelConfig>& getConfigs();
+        void setConfigs(std::vector<LabelConfig> configs);
+
+        cocos2d::CCRect getSafeZone();
+        void setSafeZone(cocos2d::CCRect rect);
+
+        bool addFromFile(std::filesystem::path path);
+
+        cocos2d::CCPoint anchorToPoint(LabelAnchor anchor);
+
+        float getSessionDuration();
+        int getAttemptCount();
+        float getFPS();
+
+        void load();
+        void save();
+};
+
+class HideLabels : public Module
+{
+    public:
+        MODULE_SETUP(HideLabels)
+        {
+            setName("Hide Labels");
+            setID("labels/hide-all-labels");
+            setDescription("Hides all the labels");
+        }
+
+        virtual void onToggle();
+};
---
+++ src/Labels/Nodes/ImageNode.cpp
@@ -0,0 +1,45 @@
+#include "ImageNode.hpp"
+
+void ImageNode::labelConfigUpdated()
+{
+    if (sprite)
+    {
+        sprite->removeFromParent();
+        sprite = nullptr;
+    }
+
+    sprite = CCSprite::create(config.imageLocation.c_str());
+
+    #if GEODE_COMP_GD_VERSION >= 22081
+    if (sprite && sprite->isUsingFallback())
+        sprite = nullptr;
+    #endif
+
+    if (sprite && sprite->getUserObject("geode.texture-loader/fallback"))
+        sprite = nullptr;
+
+    if (!sprite)
+    {
+        sprite = CCSprite::create("sog.png"_spr);
+    }
+
+    if (!sprite)
+        return;
+
+    sprite->setOpacity(config.opacity * 255);
+    sprite->setAnchorPoint(ccp(0, 0));
+    this->addChild(sprite);
+}
+
+void ImageNode::update(float dt)
+{
+    if (sprite)
+    {
+        sprite->setScale(std::min<float>(75.0f / sprite->getContentHeight(), 1));
+        this->setContentSize(sprite->getScaledContentSize());
+    }
+    else
+    {
+        this->setContentSize(ccp(0, 0));
+    }
+}
