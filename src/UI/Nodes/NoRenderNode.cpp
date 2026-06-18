d738a5730324944db37cf9137456f5b7d94a76fc
itzz <georg.ott.mail@gmail.com>
2026-06-18 06:29:18 +0200

    cooking

---
+++ src/UI/Nodes/NoRenderNode.cpp
@@ -0,0 +1,24 @@
+#include "NoRenderNode.hpp"
+#include <AndroidBall.hpp>
+#include <FloatingButton/FloatingUIManager.hpp>
+#include <Gestures/GestureManager.hpp>
+#include "../../Features/Universal/Paint/PaintNode.hpp"
+
+void qolmod::NoRenderNode::visit()
+{
+
+}
+
+$on_game(Loaded)
+{
+    auto render = qolmod::NoRenderNode::create();
+
+    render->addChild(AndroidBall::get());
+    render->addChild(FloatingUIManager::get());
+    render->addChild(qolmod::PaintNode::get());
+    render->addChild(qolmod::GestureManager::get());
+
+    #if GEODE_COMP_GD_VERSION == 22081
+    geode::OverlayManager::get()->addChild(render);
+    #endif
+};
